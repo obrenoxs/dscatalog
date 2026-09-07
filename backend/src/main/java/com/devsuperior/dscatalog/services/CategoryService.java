ac03cd317b95ce1e07e3fd46fde3254ffc748d12
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-07 00:06:16 -0300

    feat: add insert endpoint to CategoryResource

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -29,4 +29,13 @@ public class CategoryService {
                 .orElseThrow(() -> new EntityNotFoundException("Category not found"));
         return new CategoryDTO(entity);
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+
+        return new CategoryDTO(entity);
+    }
 }
